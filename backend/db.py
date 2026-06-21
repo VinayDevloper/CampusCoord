d9d76b875e8be8ba2fb6a4eb06e74ed0df117dd6
VinayDevloper <sawantvinay289@gmail.com>
2026-06-21 14:32:01 +0530

    env file access done and cloud database url added

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,11 @@
-from sqlalchemy import create_engine
+import os
 
-DATABASE_URL = "postgresql://postgres:?harekrishna?@localhost:5432/political_app_db"
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
